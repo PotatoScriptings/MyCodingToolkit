3ac2eaa3d933d2ee1b2fb1a7693d19b5ccf29c41
Gubby <aapojnivala@gmail.com>
2025-07-24 17:23:26 +0300

    Added a KeyBindInfo file to use for information modules.

---
+++ KeyBindInfo.lua
@@ -0,0 +1,13 @@
+-- Example table (delete later)
+local module = {}
+
+-- Example information
+module["Keybinds"] = {
+    -- Example keybind (delete later)
+    DoSomething = {
+        KeyCode = Enum.KeyCode.E, -- The key code for the action
+        Toggle = false, -- If the keybind is a toggle or a hold (optional, not including means it's a single press)
+    }
+
+    -- Add more keybinds as needed
+}
