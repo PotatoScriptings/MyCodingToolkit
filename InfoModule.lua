652fa1fe005abf15221bf1b3c5470f690981205b
Gubby <aapojnivala@gmail.com>
2025-07-24 15:02:58 +0300

    Added information module template.

---
+++ InfoModule.lua
@@ -0,0 +1,19 @@
+--[[
+    Date of creation: DD/MM/YYYY (DD/MM/YYYY)
+    Author: Potato
+    Purpose: Insert purpose here.
+]]
+
+-- Main table
+local module = {}
+
+-- Information template (delete later)
+module["InfoTemplate"] = {
+    -- Add more information specific settings here
+
+    -- Example: SomeSetting = 52, 
+    -- SomeOtherSetting = {
+        -- SubSetting = 10,
+        -- OtherSubSetting = "Insert name here",
+    -- }
+}
